4a935411c5414a19367194853028c9f4db8ca0bc
qoramen <tojiboyevmurodbbek@gmail.com>
2024-07-30 15:27:12 +0500

    dark-theme added and other modifications

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,26 @@
 window.addEventListener('scroll', function () {
 
     const navbar = document.querySelector('.navbar')
+    const home = document.querySelector('.home')
     if (window.scrollY >= 165) {
         navbar.classList.add('navbar-float')
+        home.classList.add('home-nav')
     } else {
         navbar.classList.remove('navbar-float')
+        home.classList.remove('home-nav')
     }
+});
+
+const darkThemeToggle = document.getElementById('dark-theme-btn');
+const darkThemeIcon = document.getElementById('icon')
+
+darkThemeToggle.addEventListener('click', function(){
+    if(darkThemeIcon.classList.contains('fa-moon')){
+        darkThemeIcon.classList.add('fa-sun');
+        darkThemeIcon.classList.remove('fa-moon');
+    } else {
+        darkThemeIcon.classList.add('fa-moon');
+        darkThemeIcon.classList.remove('fa-sun');
+    }
+    document.body.classList.toggle('dark-theme-palate')
 })
